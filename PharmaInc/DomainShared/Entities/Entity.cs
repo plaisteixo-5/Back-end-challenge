6a0cb45352d9f44a260830badaabb861d6babeae
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-10-27 21:49:11 -0300

    Adiciona a entidade e propriedades do usuario

--- PharmaInc/DomainShared/Entities/Entity.cs
+++ PharmaInc/DomainShared/Entities/Entity.cs
@@ -4,11 +4,15 @@ namespace DomainShared.Entities
 {
     public class Entity
     {
-        public Entity()
+        public Entity(string name, string value)
         {
             Id = Guid.NewGuid();
+            Name = name;
+            Value = value;
         }
 
         public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
     }
 }
