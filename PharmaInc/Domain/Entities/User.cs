f41c2b917652ba416abc51f6547e61d43bd4d316
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-10-27 21:58:03 -0300

    Adiciona o dominio de testes novamente

--- PharmaInc/Domain/Entities/User.cs
+++ PharmaInc/Domain/Entities/User.cs
@@ -4,6 +4,32 @@ namespace Domain.Entities
 {
     public class User : Entity
     {
+        public User(string gender, Name name, Location location, string email, Login login, Dob dob, Registered registered, string phone, string cell, Id id, Picture picture, Naturality nat)
+        : base(id.name, id.value)
+        {
+            Gender = gender;
+            Name = name;
+            Location = location;
+            Email = email;
+            Login = login;
+            Dob = dob;
+            Registered = registered;
+            Phone = phone;
+            Cell = cell;
+            Picture = picture;
+            Nat = nat;
+        }
 
+        public string Gender { get; set; }
+        public Name Name { get; set; }
+        public Location Location { get; set; }
+        public string Email { get; set; }
+        public Login Login { get; set; }
+        public Dob Dob { get; set; }
+        public Registered Registered { get; set; }
+        public string Phone { get; set; }
+        public string Cell { get; set; }
+        public Picture Picture { get; set; }
+        public Naturality Nat { get; set; }
     }
 }
