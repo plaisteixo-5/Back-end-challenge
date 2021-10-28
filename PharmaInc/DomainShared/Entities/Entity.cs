6e4b05981570d93b09ea12966bb14ea0ae092f41
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-10-27 23:20:08 -0300

    Remove o Id da classe Entity

--- PharmaInc/DomainShared/Entities/Entity.cs
+++ PharmaInc/DomainShared/Entities/Entity.cs
@@ -4,15 +4,5 @@ namespace DomainShared.Entities
 {
     public class Entity
     {
-        public Entity(string name, string value)
-        {
-            Id = Guid.NewGuid();
-            Name = name;
-            Value = value;
-        }
-
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Value { get; set; }
     }
 }
