0d1744e9838e677bf7ee093c0b160f27681c13c5
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-10-28 00:32:38 -0300

    Adiciona validaçoes ao ValueObject Name

--- PharmaInc/Domain/ValueObjects/Name.cs
+++ PharmaInc/Domain/ValueObjects/Name.cs
@@ -1,7 +1,20 @@
+using FluentValidator;
+using FluentValidator.Validation;
+
 namespace Domain.ValueObjects
 {
-    public class Name
+    public class Name : Notifiable
     {
+        public Name()
+        {
+            AddNotifications(
+                new ValidationContract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "FirstName", "O primeiro nome deve conter pelo menos 3 caracteres!")
+                .HasMinLen(LastName, 3, "LastName", "O segundo nome deve conter pelo menos 3 caracteres!")
+            );
+
+        }
         public string Title { get; private set; }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
