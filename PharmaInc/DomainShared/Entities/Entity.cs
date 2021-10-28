cee2e60154936fa5f0e2a823fe66917f81dd3f5a
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-10-27 23:20:48 -0300

    Adiciona o Id na entity

--- PharmaInc/DomainShared/Entities/Entity.cs
+++ PharmaInc/DomainShared/Entities/Entity.cs
@@ -4,5 +4,11 @@ namespace DomainShared.Entities
 {
     public class Entity
     {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
     }
 }
