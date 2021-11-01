067b5e38203e5c3aaec1af7945667baac0eaa106
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-11-01 08:45:38 -0300

    Adiciona a rota de update ao user-route

--- PharmaInc/src/routes/user-route.js
+++ PharmaInc/src/routes/user-route.js
@@ -5,6 +5,7 @@ const controller = require('../controllers/user-controller');
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
 router.put('/', controller.create);
+router.put('/v1', controller.updateDatabase);
 router.delete('/:id', controller.delete);
 
 module.exports = router;
